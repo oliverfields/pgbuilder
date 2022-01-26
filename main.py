0ad4a22d99f227f7bb639f7df5bfb33b2acf72d3
Oliver Fields <oliver@phnd.net>
2022-01-26 20:48:09 +0000

    a

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ async def root():
 		"message": "Hello world"
 	}
 
-@app.post('/')
 async def build():
 	return {
 		"message": "Hello post"
 	}
+@app.post('/')
+async def get_body(request: Request):
+	''' Check post request looks cosher and from viable source, if so try to download file from Dropbox and combine it with pagegen_site from github, then try to build '''
+
+	return await request.json()
